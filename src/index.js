3dfb48dbfd6ef468207663863fec545c65cc37e6
Arthurbhs <arthur.shekinarfoxy@gmail.com>
2022-10-26 12:06:50 -0300

    calculadora inicial

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Calculator from './Calculator';
+import reportWebVitals from './reportWebVitals';
+
+ ReactDOM.render(
+ <div><Calculator/>
+ </div>,document.getElementById('root'));
+ reportWebVitals();
+
