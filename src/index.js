06d3991547f423c1477637790932e31ba250825c
Arthurbhs <arthur.shekinarfoxy@gmail.com>
2022-10-27 16:19:09 -0300

    capturando valores numericos

--- src/index.js
+++ src/index.js
@@ -4,8 +4,16 @@ import './index.css';
 import Calculator from './Calculator';
 import reportWebVitals from './reportWebVitals';
 
- ReactDOM.render(
- <div><Calculator/>
- </div>,document.getElementById('root'));
- reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <div>
+      <h1>Calculadora</h1>
+      <Calculator />
+    </div>
+  </React.StrictMode>
+   ,document.getElementById('root'));
+   reportWebVitals();
+
+
 
--- src/main/Button.jsx
+++ src/main/Button.jsx
@@ -1,5 +1,17 @@
 import React from 'react'
 import './Button.css'
 
-export default props => 
-    <button className='button'>0</button>
+export default props => {
+    let Class = 'button'
+    Class += props.operation ? 'operation' : ''
+    Class += props.double ? 'double' : ''
+    Class += props.triple ? 'triple' : ''
+return(
+    <button 
+    onClick={ e => props.click && props.click(e.target.innerHTML)}
+    className={Class}>
+        {props.label}
+
+    </button>
+)
+}
